b398e735cb02c936ff1f59e7f8fc81963a5ef6c4
lzlong <lizulong_a@163.com>
2017-11-27 17:11:49 +0800

    h

--- app/src/main/java/com/xm/ib42/util/MusicPreference.java
+++ app/src/main/java/com/xm/ib42/util/MusicPreference.java
@@ -2,7 +2,8 @@ package com.xm.ib42.util;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
+
+import com.xm.ib42.entity.Album;
 
 public class MusicPreference {
 
@@ -54,80 +55,8 @@ public class MusicPreference {
 		return sharedPreferences.getInt("playmode", 0);
 	}
 
-	/**
-	 * 保存歌词大小
-	 * 
-	 * @param context
-	 * @param lrc_size
-	 *            歌词大小
-	 */
-	public void savaLrcSize(Context context, int lrc_size) {
-		sharedPreferences.edit().putInt("lrc_size", lrc_size).commit();
+	public void savePlayAlbum(Context context, Album album){
+		sharedPreferences.edit().putInt("albumId", album.getId());
 	}
 
-	/**
-	 * 获取歌词大小
-	 * 
-	 * @param context
-	 * @return int lrc_size 歌词大小
-	 */
-	public int getLrcSize(Context context) {
-		return sharedPreferences.getInt("lrc_size", 22);
-	}
-
-	/**
-	 * 保存歌词颜色
-	 * 
-	 * @param context
-	 * @param lrc_color歌词颜色
-	 */
-	public void savaLrcColor(Context context, int lrc_color) {
-		sharedPreferences.edit().putInt("lrc_color", lrc_color).commit();
-	}
-
-	/**
-	 * 获取歌词颜色小
-	 * 
-	 * @param context
-	 * @return int lrc_color 歌词颜色
-	 */
-	public int getLrcColor(Context context) {
-		return sharedPreferences.getInt("lrc_color", Color.rgb(51, 181, 229));
-	}
-
-	/**
-	 * 保存默认标签个数
-	 * 
-	 * @param context
-	 * @param tab_count 标签个数
-	 */
-	public void savaTagCount(Context context, int tab_count) {
-		sharedPreferences.edit().putInt("tab_count", tab_count).commit();
-	}
-
-	/**
-	 * 获取默认标签个数
-	 * @param context
-	 * @return int tab_count 标签个数
-	 */
-	public int getTagCount(Context context) {
-		return sharedPreferences.getInt("tab_count", 10);
-	}
-
-	/**
-	 * 获取 标签 选择的默认位置
-	 * @param data
-	 * @param context
-	 * @return
-	 */
-	public int getTagPos(String[] data, Context context) {
-		int n = 0;
-		for (int i = 0; i < data.length; i++) {
-			if (String.valueOf(getTagCount(context)).equals(data[i])) {
-				n = i;
-				break;
-			}
-		}
-		return n;
-	}
 }
